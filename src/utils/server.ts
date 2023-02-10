bc8ff21553e4e019523b184206857888383cbfda
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 20:31:52 +0530

    cors set to default

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -6,29 +6,8 @@ const createServer = () => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  // app.use(cors());
-  app.use(
-    cors({
-      credentials: true,
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-      allowedHeaders: ["Content-Type", "Authorization"],
-      origin: [
-        "http://localhost:3000",
-        "https://handcraft-git-main-muntha-swivel.vercel.app",
-        "https://main.d3gukprab816t5.amplifyapp.com",
-      ],
-    })
-  );
+  app.use(cors());
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   return app;
 };
 
