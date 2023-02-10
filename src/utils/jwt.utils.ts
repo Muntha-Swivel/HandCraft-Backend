abe3990b8b627c2b2d01f58e650cd2afd9b5802c
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 18:25:43 +0530

    added private key and public key as environment variables

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,33 +1,8 @@
 import jwt from "jsonwebtoken";
 import config from "config";
 
-// export function signJwt(
-//   object: Object,
-//   keyName: "accessTokenPrivateKey" | "refreshTokenPrivateKey",
-//   options?: jwt.SignOptions | undefined
-// ) {
-//   const signingKey = Buffer.from(
-//     config.get<string>(keyName),
-//     "base64"
-//   ).toString("ascii");
-
-//   return jwt.sign(object, signingKey, {
-//     ...(options && options),
-//     algorithm: "RS256",
-//   });
-// }
-
-// jwt.sign(
-//   { foo: "bar" },
-//   privateKey,
-//   { algorithm: "RS256" },
-//   function (err, token) {
-//     console.log(token);
-//   }
-// );
-
-const privateKey = config.get<string>("privateKey");
-const publickey = config.get<string>("publicKey");
+const privateKey = process.env.privateKey || config.get<string>("privateKey");
+const publickey = process.env.publicKey || config.get<string>("publicKey");
 
 const signJwt = (object: object, options?: jwt.SignOptions | undefined) => {
   return jwt.sign(object, privateKey, {
@@ -55,28 +30,3 @@ const verifyJwt = (token: string) => {
 };
 
 export { signJwt, verifyJwt };
-
-// export function verifyJwt(
-//   token: string,
-//   keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
-// ) {
-//   const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString(
-//     "ascii"
-//   );
-
-//   try {
-//     const decoded = jwt.verify(token, publicKey);
-//     return {
-//       valid: true,
-//       expired: false,
-//       decoded,
-//     };
-//   } catch (e: any) {
-//     console.error(e);
-//     return {
-//       valid: false,
-//       expired: e.message === "jwt expired",
-//       decoded: null,
-//     };
-//   }
-// }
