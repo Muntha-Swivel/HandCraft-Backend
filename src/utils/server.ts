be7ae007cd8e711fe4bcb8dccc2ad422c48dbbba
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 20:03:41 +0530

    changed some cors settings

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -9,14 +9,26 @@ const createServer = () => {
   // app.use(cors());
   app.use(
     cors({
-      origin: [
-        "https://handcraft-git-main-muntha-swivel.vercel.app",
-        "http://localhost:3000",
-      ],
       credentials: true,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+      allowedHeaders: ["Content-Type", "Authorization"],
+      origin: [
+        "http://localhost:3000",
+        "https://handcraft-git-main-muntha-swivel.vercel.app",
+        "https://main.d3gukprab816t5.amplifyapp.com",
+      ],
     })
   );
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   return app;
 };
 
