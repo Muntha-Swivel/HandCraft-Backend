ed2925c2bf943e633592908b6dfa714620d6cf56
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-11 23:02:22 +0530

    changed cors settings

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -6,16 +6,37 @@ const createServer = () => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
-  app.use(
-    cors({
-      origin: [
-        "https://handcraft-git-main-muntha-swivel.vercel.app/",
-        "http://localhost:300",
-      ],
-      credentials: true,
-    })
-  );
+  // // app.use(cors());
+  // // app.use(
+  // //   cors({
+  // //     origin: ["http://localhost:3000"],
+  // //     credentials: true,
+  // //   })
+  // // );
+  // app.use(function (req, res, next) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //   next();
+  // });
+
+  const corsOptions = {
+    origin: "https://handcraft-git-main-muntha-swivel.vercel.app",
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
+
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://handcraft-git-main-muntha-swivel.vercel.app"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 
   return app;
 };
