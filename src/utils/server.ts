c6fd3971de0d5a924e60ea92b8457f17bfeb64f8
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-13 11:39:46 +0530

    changed cors policy

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -19,17 +19,23 @@ const createServer = () => {
   //   res.header('Access-Control-Allow-Headers', 'Content-Type');
   //   next();
   // });
+  const corsOptions = {
+    origin: "https://handcraft.vercel.app",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://handcraft.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+  // app.use(function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "https://handcraft.vercel.app");
+  //   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept"
+  //   );
+  //   res.header("Access-Control-Allow-Credentials", "true");
+  //   next();
+  // });
 
   return app;
 };
