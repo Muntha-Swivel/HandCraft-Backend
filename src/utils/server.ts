bb5f3f47877d00e1a1f50a85663409e9295e0f29
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-13 09:56:19 +0530

    changed cors policy

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -30,7 +30,10 @@ const createServer = () => {
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://handcraft.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
