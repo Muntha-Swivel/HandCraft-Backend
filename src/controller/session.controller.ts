73a4b8b2d2be4a7b883f83d12e1b30b5bb57ff6c
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-14 12:24:04 +0530

    changed cors options

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -33,10 +33,14 @@ const createUserSessionHandler = async (req: Request, res: Response) => {
   // return access & refresh tokens
   res.cookie("accessToken", accessToken, {
     maxAge: 300000, // 5 minutes
+    sameSite: "none",
+    secure: true,
   });
   res.cookie("refreshToken", refreshToken, {
     maxAge: 300000, // 5 minutes
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
 
   return res.send({ accessToken, refreshToken });
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,41 +1,19 @@
 import express from "express";
 import cors from "cors";
-//import router from "../routes";
+import cookieParser from "cookie-parser";
 
 const createServer = () => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  //app.use(cors());
-  // app.use(
-  //   cors({
-  //     origin: ["https://handcraft.vercel.app/"],
-  //     credentials: true,
-  //   })
-  // );
-  // app.use(function (req, res, next) {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  //   next();
-  // });
+  app.use(cookieParser());
+
   const corsOptions = {
     origin: "https://handcraft.vercel.app",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   };
   app.use(cors(corsOptions));
-
-  // app.use(function (req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "https://handcraft.vercel.app");
-  //   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  //   res.header(
-  //     "Access-Control-Allow-Headers",
-  //     "Origin, X-Requested-With, Content-Type, Accept"
-  //   );
-  //   res.header("Access-Control-Allow-Credentials", "true");
-  //   next();
-  // });
 
   return app;
 };
