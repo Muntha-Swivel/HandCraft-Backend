864e87446fa061dd569100edb9d3e084f676bfd9
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-15 17:32:33 +0530

    small changes

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -15,17 +15,6 @@ const createServer = () => {
   };
   app.use(cors(corsOptions));
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://handcraft.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
-
   return app;
 };
 
