b649d81132a3ecb16e04002cef770c335d2d3912
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-15 12:27:40 +0530

    changed cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -8,24 +8,12 @@ const createServer = () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
 
-  const corsOptions = {
-    origin: "https://handcraft.vercel.app",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  };
-
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-    );
-    next();
-  });
-
-  app.use(cors(corsOptions));
+  app.use(
+    cors({
+      origin: ["https://handcraft.vercel.app/"],
+      credentials: true,
+    })
+  );
 
   console.log("cors working");
 
